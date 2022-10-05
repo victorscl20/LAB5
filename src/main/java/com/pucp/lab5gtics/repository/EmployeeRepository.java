8da9bcd0b23e804210a9f2d51d5d5a271f34815a
MarceloJon <mrojasl@pucp.edu.pe>
2022-10-04 21:00:15 -0500

    avance gaaa

--- src/main/java/com/pucp/lab5gtics/repository/EmployeeRepository.java
+++ src/main/java/com/pucp/lab5gtics/repository/EmployeeRepository.java
@@ -1,8 +1,13 @@
 package com.pucp.lab5gtics.repository;
 
+import com.pucp.lab5gtics.controller.EmployeeController;
+import com.pucp.lab5gtics.entity.EmployeesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
 
 //Completar
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<EmployeesEntity, Integer> {
 }
