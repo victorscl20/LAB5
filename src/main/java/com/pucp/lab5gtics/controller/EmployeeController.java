957d63872ef0389a0b1f95e927276f0c6e7a4af3
MarceloJon <mrojasl@pucp.edu.pe>
2022-10-04 21:13:40 -0500

    gaaaaaaaaaaaaaa

--- src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
@@ -5,19 +5,17 @@ import com.pucp.lab5gtics.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @GetMapping({"empleado/lista", "empleado"})
+    @GetMapping({"lista", ""})
     public String listEmployee(Model model,
                                @RequestParam(name = "search",required = false) String search,
                                @RequestParam(name = "order", required = false) Integer order,
@@ -45,10 +43,10 @@ public class EmployeeController {
 
     //Guardar Empleado
     //@...Mapping("")
-    @PostMapping("empleado/guardar")
+    @PostMapping("guardar")
     public String saveEmployee(EmployeesEntity employeesEntity) {
         employeeRepository.save(employeesEntity);
-        return "redirect:/empleado";
+        return "redirect:/employee";
     }
 
     //Nuevo Empleado
