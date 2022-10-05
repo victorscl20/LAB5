26ed6f695e39e9069e0ad29cc1eb21a953207b97
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2022-10-04 21:08:31 -0500

    login uu

--- src/main/java/com/pucp/lab5gtics/controller/HomeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.pucp.lab5gtics.controller;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.net.Authenticator;
 
 @Controller
 public class HomeController {
@@ -9,7 +14,37 @@ public class HomeController {
     //Home
     @GetMapping(value = {"/","/login"})
     public String login(){
-
         return "inicio";
     }
+
+    @GetMapping(value = "/redirectCategoria")
+    public String redirectByCategoria(Authentication auth){
+        String categoria = "";
+        for(GrantedAuthority role : auth.getAuthorities()){
+            categoria = role.getAuthority();
+            break;
+        }
+        if(categoria.equals("1")||categoria.equals("0")){
+            categoria = "general";
+        }
+
+        switch (categoria){
+            case "general":
+                return "redirect:/empleado/lista";
+            case "1":
+                return "redirect:/empleado/";
+            case "0":
+                return "redirect:/empleado/guardar";
+            default:
+                return "redirect:/login/";
+
+        }
+
+
+
+
+
+
+    }
+
 }
